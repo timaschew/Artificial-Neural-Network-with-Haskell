02701e4a6828d594ed998dba604c9ed25204ac91
Anton Wilhelm <timaschew@gmail.com>
2010-05-04 19:29:12 +0200

    Neuron data type, read each line and cast String to Double

---
+++ src/exercises/01/Neuron2.hs
@@ -0,0 +1,29 @@
+import System.IO
+
+data Neuron = Neuron {	treashold :: Double
+					, weights :: [Double]
+					} deriving (Show)
+
+main = do
+
+	inh <- openFile "neuron" ReadMode
+	let l = [] :: [Double]
+	readLineLoop inh l
+	print l
+	
+	-- don'work yet
+	--let threshold = (lines contents) !! 1
+	--let n = Neuron threshold l
+	--print "fertig"
+	
+readLineLoop :: Handle -> [Double] -> IO ()
+readLineLoop inh list = do 
+	is_eof <- hIsEOF inh
+	if is_eof
+		then return()
+		else do
+			inpStr <- hGetLine inh
+			let val = read inpStr :: Double
+			putStrLn inpStr
+			readLineLoop inh (val:list)
+
