d2e154bc5045228e30c745149e3da8d6960d9221
Anton Wilhelm <timaschew@gmail.com>
2010-06-17 12:37:08 +0200

    add new font

--- data/traindata/img/raw/converting.sh
+++ data/traindata/img/raw/converting.sh
@@ -1,19 +1,21 @@
 # usage
-# ./converting.sh 14 18 pgm14_18
-# converts all images (jpg files) of current path into pgm (portable grayscale) in the folder pgm14_18 with a rescale: width=14 height=18
-# original files have are 70px weight x 90px height 
+# ./converting.sh inp 14 18 pgm14_18
+# converts all images (jpg files) of "inp" dir into pgm (portable grayscale) in the folder pgm14_18$
+# use directories without slash !
+# original files have are 70px weight x 90px height
 
-width=$1
-height=$2
-res=$1x$2!
+width=$2
+height=$3
+res=$2x$3!
 
-outputdir=$3
+srcdir=$1
+outputdir=$4
 mkdir $outputdir
 
-for i in $(find . -name "*.jpg"); do
-	filename=${i//.\//""}
-	new_file=${filename//jpg/pgm}
-	#echo $filename wird zu $new_file
-	convert $filename -resize $res -compress none $outputdir/$new_file
-	#convert A.jpg -resize 35x45! -compress none A_big.pgm
+for i in $(find $srcdir -name "*.jpg" -depth 1); do
+        filename=${i//$srcdir\//""}
+        new_file=${filename//jpg/pgm}
+        #echo $srcdir/$filename wird zu $outputdir/$new_file
+        convert $srcdir/$filename -resize $res -compress none $outputdir/$new_file
+        #convert A.jpg -resize 35x45! -compress none A_big.pgm
 done
